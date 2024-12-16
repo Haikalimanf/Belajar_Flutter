18d1de45783db45a23a4a1a6763d4084f59cffcc
Haikalimanf <haikalimanf@gmail.com>
2024-12-16 14:14:41 +0700

    Menambahkan project list view

--- lib/main.dart
+++ lib/main.dart
@@ -12,159 +12,36 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'First Screen',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const FirstScreen(),
+      home: ScrollingScreen(),
     );
   }
 }
 
-class FirstScreen extends StatefulWidget {
-  const FirstScreen({super.key});
+class ScrollingScreen extends StatelessWidget {
+  final List<int> numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-  @override
-  State<FirstScreen> createState() => _FirstScreenState();
-}
-
-class _FirstScreenState extends State<FirstScreen> {
-  final TextEditingController _controller = TextEditingController();
-  bool lightOn = false;
-  String? language;
-  bool agree = false;
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  void _showSnackBar(String message) {
-    ScaffoldMessenger.of(context).hideCurrentSnackBar();
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text(message),
-        duration: const Duration(seconds: 1),
-      ),
-    );
-  }
+  ScrollingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('First Screen'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextField(
-              controller: _controller,
-              decoration: const InputDecoration(
-                hintText: 'Write your name here...',
-                labelText: 'Your Name',
+      body: ListView.builder(
+        itemCount: numberList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            height: 250,
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              border: Border.all(color: Colors.black),
+            ),
+            child: Center(
+              child: Text(
+                '${numberList[index]}',
+                style: const TextStyle(fontSize: 50),
               ),
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                if (_controller.text.isNotEmpty) {
-                  showDialog(
-                    context: context,
-                    builder: (context) {
-                      return AlertDialog(
-                        title: const Text('Welcome'),
-                        content: Text('Hello, ${_controller.text}'),
-                        actions: [
-                          TextButton(
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                            child: const Text('OK'),
-                          ),
-                        ],
-                      );
-                    },
-                  );
-                } else {
-                  _showSnackBar('Please enter your name!');
-                }
-              },
-              child: const Text('Submit'),
-            ),
-            const SizedBox(height: 20),
-            Row(
-              children: [
-                const Text('Light:'),
-                Switch(
-                  value: lightOn,
-                  onChanged: (bool value) {
-                    setState(() {
-                      lightOn = value;
-                    });
-                    _showSnackBar(lightOn ? 'Light On' : 'Light Off');
-                  },
-                ),
-              ],
-            ),
-            const SizedBox(height: 20),
-            const Text('Select your favorite language:'),
-            Column(
-              children: [
-                ListTile(
-                  leading: Radio<String>(
-                    value: 'Dart',
-                    groupValue: language,
-                    onChanged: (String? value) {
-                      setState(() {
-                        language = value;
-                      });
-                      _showSnackBar('$language selected');
-                    },
-                  ),
-                  title: const Text('Dart'),
-                ),
-                ListTile(
-                  leading: Radio<String>(
-                    value: 'Kotlin',
-                    groupValue: language,
-                    onChanged: (String? value) {
-                      setState(() {
-                        language = value;
-                      });
-                      _showSnackBar('$language selected');
-                    },
-                  ),
-                  title: const Text('Kotlin'),
-                ),
-                ListTile(
-                  leading: Radio<String>(
-                    value: 'Swift',
-                    groupValue: language,
-                    onChanged: (String? value) {
-                      setState(() {
-                        language = value;
-                      });
-                      _showSnackBar('$language selected');
-                    },
-                  ),
-                  title: const Text('Swift'),
-                ),
-                ListTile(
-                  leading: Checkbox(
-                    value: agree,
-                    onChanged: (bool? value) {
-                      setState(() {
-                        agree = value!;
-                      });
-                      _showSnackBar('Apakah setujuh? $agree ');
-                    },
-                  ),
-                  title: const Text('Agree / Disagree'),
-                ),
-              ],
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
