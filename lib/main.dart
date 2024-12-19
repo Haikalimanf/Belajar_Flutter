0d2264480ee6ed2d07bd6551b1357e14218bbafe
Haikalimanf <haikalimanf@gmail.com>
2024-12-19 14:15:58 +0700

    feat: menambahkan contoh penggunaan navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:belajar_flutter_dicoding/secondScreen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,145 +13,29 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'First Screen',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const ExpandedFlexiblePage(),
+      home: const FirstScreen(),
     );
   }
 }
 
-// class Rainbow extends StatelessWidget {
-//   const Rainbow({super.key});
+class FirstScreen extends StatelessWidget {
+  final String message = 'Hello from First Screen!';
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Column(
-//       children: <Widget>[
-//         Expanded(
-//           child: Container(
-//             color: Colors.red,
-//           ),
-//         ),
-//         Expanded(
-//           flex: 2,
-//           child: Container(
-//             color: Colors.orange,
-//           ),
-//         ),
-//         Expanded(
-//           child: Container(
-//             color: Colors.yellow,
-//           ),
-//         ),
-//         Expanded(
-//           child: Container(
-//             color: Colors.green,
-//           ),
-//         ),
-//         Expanded(
-//           child: Container(
-//             color: Colors.blue,
-//           ),
-//         ),
-//         Expanded(
-//           child: Container(
-//             color: Colors.indigo,
-//           ),
-//         ),
-//         Expanded(
-//           child: Container(
-//             color: Colors.purple,
-//           ),
-//         ),
-//       ],
-//     );
-//   }
-// }
-
-class ExpandedFlexiblePage extends StatelessWidget {
-  const ExpandedFlexiblePage({super.key});
+  const FirstScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Row(
-              children: [
-                ExpandedWidget(),
-                FlexibleWidget(),
-              ],
-            ),
-            Row(
-              children: [
-                ExpandedWidget(),
-                ExpandedWidget(),
-              ],
-            ),
-            Row(
-              children: [
-                FlexibleWidget(),
-                FlexibleWidget(),
-              ],
-            ),
-            Row(
-              children: [
-                FlexibleWidget(),
-                ExpandedWidget(),
-              ],
-            ),
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Screen'),
       ),
-    );
-  }
-}
-
-class ExpandedWidget extends StatelessWidget {
-  const ExpandedWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.teal,
-          border: Border.all(color: Colors.white),
-        ),
-        child: const Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Text(
-            'Expanded',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 24,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class FlexibleWidget extends StatelessWidget {
-  const FlexibleWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Flexible(
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.tealAccent,
-          border: Border.all(color: Colors.white),
-        ),
-        child: const Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Text(
-            'Flexible',
-            style: TextStyle(
-              color: Colors.teal,
-              fontSize: 24,
-            ),
-          ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Pindah Screen'),
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => SecondScreen(message)));
+          },
         ),
       ),
     );
