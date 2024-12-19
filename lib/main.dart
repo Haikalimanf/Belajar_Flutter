27cecfa43b9161ce7ad6b934aef75358a885996a
Haikalimanf <haikalimanf@gmail.com>
2024-12-19 13:41:42 +0700

    feat: menambahkan contoh dari componen  expanded dan flexibel

--- lib/main.dart
+++ lib/main.dart
@@ -12,36 +12,146 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'First Screen',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: ScrollingScreen(),
+      home: const ExpandedFlexiblePage(),
     );
   }
 }
 
-class ScrollingScreen extends StatelessWidget {
-  final List<int> numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// class Rainbow extends StatelessWidget {
+//   const Rainbow({super.key});
 
-  ScrollingScreen({super.key});
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       children: <Widget>[
+//         Expanded(
+//           child: Container(
+//             color: Colors.red,
+//           ),
+//         ),
+//         Expanded(
+//           flex: 2,
+//           child: Container(
+//             color: Colors.orange,
+//           ),
+//         ),
+//         Expanded(
+//           child: Container(
+//             color: Colors.yellow,
+//           ),
+//         ),
+//         Expanded(
+//           child: Container(
+//             color: Colors.green,
+//           ),
+//         ),
+//         Expanded(
+//           child: Container(
+//             color: Colors.blue,
+//           ),
+//         ),
+//         Expanded(
+//           child: Container(
+//             color: Colors.indigo,
+//           ),
+//         ),
+//         Expanded(
+//           child: Container(
+//             color: Colors.purple,
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+// }
+
+class ExpandedFlexiblePage extends StatelessWidget {
+  const ExpandedFlexiblePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView.builder(
-        itemCount: numberList.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Container(
-            height: 250,
-            decoration: BoxDecoration(
-              color: Colors.grey,
-              border: Border.all(color: Colors.black),
+    return const Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                ExpandedWidget(),
+                FlexibleWidget(),
+              ],
             ),
-            child: Center(
-              child: Text(
-                '${numberList[index]}',
-                style: const TextStyle(fontSize: 50),
-              ),
+            Row(
+              children: [
+                ExpandedWidget(),
+                ExpandedWidget(),
+              ],
             ),
-          );
-        },
+            Row(
+              children: [
+                FlexibleWidget(),
+                FlexibleWidget(),
+              ],
+            ),
+            Row(
+              children: [
+                FlexibleWidget(),
+                ExpandedWidget(),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ExpandedWidget extends StatelessWidget {
+  const ExpandedWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.teal,
+          border: Border.all(color: Colors.white),
+        ),
+        child: const Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Text(
+            'Expanded',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class FlexibleWidget extends StatelessWidget {
+  const FlexibleWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.tealAccent,
+          border: Border.all(color: Colors.white),
+        ),
+        child: const Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Text(
+            'Flexible',
+            style: TextStyle(
+              color: Colors.teal,
+              fontSize: 24,
+            ),
+          ),
+        ),
       ),
     );
   }
